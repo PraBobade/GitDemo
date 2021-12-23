a0fad14282585bbaad3579d25a780fbab45a3722
Pradip Bobade <bobadepradip18@gmail.com>
2021-12-23 13:57:30 +0530

    first commit

---
+++ src/test/java/org/example/AppiumTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+    @Test
+    public void Mobile(){
+        System.out.println("This is my mobile");
+    }
+
+    @Test
+    public void Thisisapp(){
+        System.out.println("This is My app");
+    }
+}
---
+++ src/test/java/org/example/SeleniumTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    @Test
+    public void twitterDelete(){
+        System.out.println("twitter deleted successfully");
+    }
+    @Test
+    public void elements(){
+        System.out.println("This is element");
+    }
+    @Test
+    public void elemen(){
+        System.out.println("This is element2323");
+    }
+}
